7690c808bf6e4b0c45d9e03cc0eb6dc140ef6682
singhsunny26sep <101707682+singhsunny26sep@users.noreply.github.com>
2026-05-09 15:45:54 +0530

    new

--- android/app/src/main/java/com/nvsricemart/MainActivity.kt
+++ android/app/src/main/java/com/nvsricemart/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nvsricemall
+package com.nvsricemart
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "nvsricemall"
+  override fun getMainComponentName(): String = "nvsricemart"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
